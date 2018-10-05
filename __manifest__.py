493da8054251722e4109b76059481c9e25131b49
Eduardo C <dev.eduardoo@outlook.com>
2018-10-05 11:28:23 -0500

    created a snippet

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
   'version': '1.0',
   'author': 'Ed',
   'data': [
-    'views/layout.xml', 'views/pages.xml', 'views/assets.xml'
+    'views/layout.xml', 'views/pages.xml', 'views/assets.xml', 'views/snippets.xml'
   ],
   'category': 'Theme/Creative',
   'depends': ['website'],

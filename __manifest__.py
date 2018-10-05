c65e2e5ee0012a8c7e8ffbf8c3ee809737e47d68
Eduardo C <dev.eduardoo@outlook.com>
2018-10-05 10:44:09 -0500

    created page layout

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
   'version': '1.0',
   'author': 'Ed',
   'data': [
-    'views/layout.xml'
+    'views/layout.xml', 'views/pages.xml'
   ],
   'category': 'Theme/Creative',
   'depends': ['website'],
